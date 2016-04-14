4a455ca24c8f67bd961668d3b607feed517cc990
serj <serj@serj-w81.msk.bonasource.ru>
2016-04-14 11:42:55 +0300

    merge with coach

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ import IndexPage from './RouteHandlers/IndexPage'
 import Articles from './RouteHandlers/Articles'
 import ArticlePage from './RouteHandlers/ArticlePage'
 import NewArticlePage from './RouteHandlers/NewArticlePage'
+import CommentList from './RouteHandlers/CommentList'
 
 export default (
     <Router history = {browserHistory} >
         <Route path="/" component = {IndexPage} >
+            <Route path = "comments" component = {CommentList} />
             <Route path = "articles/new" component = {NewArticlePage} />
             <Route path = "articles" component = {Articles} >
                 <Route path = "/new" component = {NewArticlePage} />

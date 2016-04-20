9842baef1f7ed338f8acd5d29a90a24662cc9a67
serj <serj@serj-w81.msk.bonasource.ru>
2016-04-20 12:05:16 +0300

    logs deleted

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -4,11 +4,8 @@ import {articles as defaultArticles} from '../fixtures'
 export default (articles = defaultArticles, action) => {
     const { type, data } = action
 
-    console.log('## articles reducer', type)
-
     switch (type) {
         case DELETE_ARTICLE:
-          console.log(DELETE_ARTICLE, data)
           return articles.filter((item) => item.id != data.articleId)
         break;
     }

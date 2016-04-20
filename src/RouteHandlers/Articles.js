e780656a680d14c1d70d14af525ad7fc4e483585
serj <serj@serj-w81.msk.bonasource.ru>
2016-04-20 12:15:07 +0300

    merge with coach

--- src/RouteHandlers/Articles.js
+++ src/RouteHandlers/Articles.js
@@ -7,14 +7,40 @@ class Articles extends Component {
 
     };
 
+    static childContextTypes = {
+        style: PropTypes.object
+    }
+
+    state = {
+        style: { color: 'green'}
+    }
+
+    getChildContext() {
+        return this.state
+    }
+
     render() {
         return (
             <div>
-                <Link to="/articles/new">New article</Link>
+                <ul>
+                    <li><a href="#" onClick = {this.changeColor('red')}>red</a></li>
+                    <li><a href="#" onClick = {this.changeColor('green')}>green</a></li>
+                    <li><a href="#" onClick = {this.changeColor('blue')}>blue</a></li>
+                </ul>
+                <h3 onClick={this.redirectToNew}>New article</h3>
                 <Navigation />
                 {this.props.children}
             </div>
         )
+    }
+
+    changeColor = color => ev => {
+        ev.preventDefault()
+        this.setState({ style: { color } })
+    }
+
+    redirectToNew = (ev) => {
+        this.props.history.push('/articles/new')
     }
 }
 
